d767445889568805c875559e87544436d36b7d30
DmytriiGrishin <dmytriigrishin@gmail.com>
2017-06-20 03:12:55 +0300

    bug fix

--- dist/assets/lab9.js
+++ dist/assets/lab9.js
@@ -916,6 +916,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("lab9/app")["default"].create({"name":"lab9","version":"0.0.0+9bf5d7f1"});
+  require("lab9/app")["default"].create({"name":"lab9","version":"0.0.0+c4190cb6"});
 }
 //# sourceMappingURL=lab9.map
